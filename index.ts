bb74d5bfac1f25c5f78975fa9c7bd6a0083435fc
hooss <advance138138@gmail.com>
2020-03-06 17:40:04 +0900

    기본적인 타스 문법 알아가는 시간

---
+++ index.ts
@@ -0,0 +1,9 @@
+const name = "hooss",
+    age = 14,
+    gender = "male";
+
+const sayHI = (name, age, gender) => {
+    console.log(`Hello ${name}, You are ${age}, You are a ${gender}`);
+}
+
+sayHI(name, age, gender);
