4793ef658cc40b76136beea687157b11388683a9
hooss <advance138138@gmail.com>
2020-03-06 20:48:18 +0900

    블록 체인 크리에이트 완성
    아무튼 그럼

--- dist/index.js
+++ dist/index.js
@@ -16,5 +16,14 @@ let blockchain = [genesisBlcok];
 const getBlockchain = () => blockchain;
 const getLatestBlcok = () => blockchain[blockchain.length - 1];
 const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlcok();
+    const newIndex = previousBlock.index + 1;
+    const newTimeStamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
+    return newBlock;
+};
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 console.log(blockchain);
 //# sourceMappingURL=index.js.map
