73524a432e9f9a76f6459d43f697eb29e85f1718
hooss <advance138138@gmail.com>
2020-03-06 19:11:25 +0900

    part 1
    파트 1 완성

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-class Human{
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;    
     }
 }
 
-const person = {
-    name: "Hooss",
-    age: 14,
-    gender: "male"
-};
+const genesisBlcok:Block = new Block(0, "20202020202", "", "Hello", 123456);
 
-const lynn = new Human("lynn", 18, "female");
+let blockchain: [Block] = [genesisBlcok];
 
-const sayHI = (person: Human): string => {
-    return (`Hello ${person.name}, You are ${person.age}, You are a ${person.gender}!`);
-};
-
-console.log(sayHI(lynn));
+console.log(blockchain);
 
 export {}
